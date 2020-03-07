be438636d7b1ca2f5e6fc83bd9f603c61625e15b
Miguel Gondres <miguelgondres2017@ucne.edu.do>
2020-03-07 05:39:36 -0400

    Agregue todos los metodos de la BLL

--- BLL/PersonasBll.cs
+++ BLL/PersonasBll.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using RegistroPersonasExtra.DAL;
@@ -30,5 +32,95 @@ namespace RegistroPersonasExtra.BLL
 
             return paso;
         }
+
+        public static bool Modificar(Personas persona)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(persona).State = EntityState.Modified;
+                paso = (contexto.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static Personas Buscar(int id)
+        {
+            Personas persona = new Personas();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                persona = contexto.Personas.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return persona;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var eliminar = contexto.Personas.Find(id);
+                contexto.Entry(eliminar).State = EntityState.Deleted;
+                paso = (contexto.SaveChanges() > 0);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static List<Personas> GetList(Expression<Func<Personas, bool>> persona)
+        {
+            List<Personas> lista = new List<Personas>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Personas.Where(persona).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+
+
+
+
     }
 }
