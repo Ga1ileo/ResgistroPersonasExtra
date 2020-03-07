4b7e377d7848c4c7796971b34bf65db0a3cc361f
Miguel Gondres <miguelgondres2017@ucne.edu.do>
2020-03-07 05:29:41 -0400

    Cree la Carpeta DAL que contiene con su respectivo contexto, luego agregue el metodo guardar en la BLL, para asegurarme que todo estaba bien enlazado.

--- BLL/PersonasBll.cs
+++ BLL/PersonasBll.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
+using RegistroPersonasExtra.DAL;
+using RegistroPersonasExtra.Entidades;
 
 namespace RegistroPersonasExtra.BLL
 {
     public class PersonasBll
     {
+        public static bool Guardar(Personas persona)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
 
+            try
+            {
+                if (contexto.Personas.Add(persona) != null)
+                    paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
     }
 }
---
+++ DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using RegistroPersonasExtra.Entidades;
+
+namespace RegistroPersonasExtra.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Personas> Personas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DAL\Scripts\Personas.db");
+        }
+    }
+}
